79e1abc38e9286f3f58adb1d9a9230eec8eb67ec
shaktigurung <mr.shakti.gurung@gmail.com>
2019-03-14 10:21:45 +1100

    answer update

--- src/components/QuestionsAnswers.js
+++ src/components/QuestionsAnswers.js
@@ -6,11 +6,12 @@ export default () => {
     <div>
         <h4> What were the main bugs and fixes for the given Code ? </h4>
         <ul className = "question-answer">
-            <li> Function declarations were wrong, so re-defined all functions using ES6 Fat Arrow function </li>
+            <li> Re-defined all functions and methods using ES6 Fat Arrow function </li>
             <li> Assignment of value to "secondsElapsed" variable was missing "this" object</li>
-            <li> HandleresetClick function was using "," instead of ";" after laps value reset</li>
+            <li> HandleresetClick function was reseting laps value outside of setState</li>
             <li> formattedSeconds function was called on H1 tag without "this" object </li>
-            <li> Also I tried to separate various codes into functional components</li>
+            <li> handleLabClick was using forceupdate which we donot need since we using setState</li>
+            <li> Also I tried to separate various codes into functional components but due to lack of time I couldn't refactor all the codes</li>
         </ul>
 
         <h2> Thank you for your valuable time</h2>
